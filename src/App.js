b15c147adf2d9f42082bb891c660393bcaa447c5
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-11 23:29:45 +0600

    react router installation done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2 className="text-4xl text-ind">Hello Tailwind</h2>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+
+const Header = () => {
+  return (
+    <div className="header bg-emerald-500 text-white h-20">
+      <Navigation></Navigation>
+    </div>
+  );
+};
+
+export default Header;
