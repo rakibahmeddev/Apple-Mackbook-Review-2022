4494ef07f5c39951f117ef12d18a2c61d836e3f8
rakib ahmed <rakibahmed.dev@gmail.com>
2022-04-11 23:16:45 +0600

    Readme file edited

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2 className="text-4xl text-indigo-700">Hello Tailwind</h2>
+      <h2 className="text-4xl text-ind">Hello Tailwind</h2>
     </div>
   );
 }
